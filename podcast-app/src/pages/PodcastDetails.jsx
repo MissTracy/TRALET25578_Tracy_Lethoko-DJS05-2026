706e8ca31643f16f87d19e61c2927501eed99da4
Tracy Lethoko <tracylethoko@Tracys-MacBook-Pro.local>
2026-07-03 15:56:06 +0200

    added all podcast details

--- podcast-app/src/pages/PodcastDetails.jsx
+++ podcast-app/src/pages/PodcastDetails.jsx
@@ -3,13 +3,18 @@
 
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./PodcastDetails.module.css";
+import { formatDate } from "../utils/formatDate";
 
 export default function PodcastDetails() {
     const { id } = useParams();
     const [podcast, setPodcast] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const navigate = useNavigate();
+ 
+    
 
     useEffect(() => {
         async function fetchPodcast() {
@@ -46,9 +51,32 @@ export default function PodcastDetails() {
         return <p>Podcast not found.</p>;
       }
 
+      const totalEpisodes = podcast.seasons.reduce(
+        (total, season) => total + season.episodes.length,
+        0
+      );
+      const genreTags = podcast.genres
+        .filter((genre) => genre !== "All" && genre !== "Featured")
+        .map((genre) => (
+            <span key={genre} className={styles.genreTag}>
+            {genre}
+            </span>
+      ));
+
+
+    //   console.log(podcast);
+    //   console.log(podcast.genres);
+
+
       return (
         <main className={styles.container}>
+      
+          <button onClick={() => navigate(-1)}>
+            ← Back to Podcasts
+          </button>
+      
           <section className={styles.hero}>
+      
             <img
               className={styles.image}
               src={podcast.image}
@@ -56,15 +84,40 @@ export default function PodcastDetails() {
             />
       
             <div className={styles.content}>
-              <h1 className={styles.title}>{podcast.title}</h1>
+      
+              <h1>{podcast.title}</h1>
       
               <p>{podcast.description}</p>
       
+              <div>
+                <h3>Genres</h3>
+                {genreTags}
+              </div>
+      
               <p>
-                <strong>Seasons:</strong> {podcast.seasons.length}
+                <strong>Last Updated:</strong>
+                {" "}
+                {formatDate(podcast.updated)}
               </p>
+      
+              <p>
+                <strong>Total Seasons:</strong>
+                {" "}
+                {podcast.seasons.length}
+                {" Seasons"}
+             </p>
+      
+              <p>
+                <strong>Total Episodes:</strong>
+                {" "}
+                {totalEpisodes}
+                {" Episodes"}
+              </p>
+      
             </div>
+      
           </section>
+      
         </main>
       );
 }
