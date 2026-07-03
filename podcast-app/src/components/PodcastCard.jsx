bb482f6b3810d4254d23a1f35905b115ede87263
Tracy Lethoko <tracylethoko@Tracys-MacBook-Pro.local>
2026-07-03 14:53:51 +0200

    added link to podcast card and stle to it's .css file

--- podcast-app/src/components/PodcastCard.jsx
+++ podcast-app/src/components/PodcastCard.jsx
@@ -1,6 +1,6 @@
 import { formatDate } from "../utils/formatDate";
 import styles from "./PodcastCard.module.css";
-
+import { Link } from "react-router-dom";
 /**
  * Renders a single podcast preview card with image, title, number of seasons,
  * genres (as styled tags), and the last updated date.
@@ -27,15 +27,20 @@ export default function PodcastCard({ podcast, genres }) {
   });
 
   return (
-    <div className={styles.card}>
-      <img src={podcast.image} alt={podcast.title} />
+    <Link
+      to={`/show/${podcast.id}`}
+      className={styles.cardLink}
+    >
+      <div className={styles.card}>
+        <img src={podcast.image} alt={podcast.title} />
 
-      <h3>{podcast.title}</h3>
-      <p className={styles.seasons}>{podcast.seasons} seasons</p>
-      <div className={styles.tags}>{genreSpans}</div>
-      <p className={styles.updatedText}>
-        Updated {formatDate(podcast.updated)}
-      </p>
-    </div>
+        <h3>{podcast.title}</h3>
+        <p className={styles.seasons}>{podcast.seasons} seasons</p>
+        <div className={styles.tags}>{genreSpans}</div>
+        <p className={styles.updatedText}>
+          Updated {formatDate(podcast.updated)}
+        </p>
+      </div>
+    </Link>
   );
 }
